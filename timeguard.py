0df5669d733269775e5669f0d6de98f8214653a4
Richard Pearce <richard.pearce@automationlogic.com>
2020-07-27 00:42:07 +0100

    Split TimeSlots from Program

--- timeguard.py
+++ timeguard.py
@@ -8,27 +8,23 @@ import yaml
 import json
 from pathlib import Path
 
-class Program:
-  id = None
-  name = None
+""" A single time entry when the device should be on or off """
+class TimeSlot:
   attributes = {}
   DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-  def __init__(self, id, name, attributes):
-    self.id = id
-    self.name = name
+  def __init__(self, attributes):
     self.attributes = attributes
 
   def translate_days_value(self, days):
     translated_days = []
     for dow in days:
-      if days[dow] == '1': 
-        translated_days.append(self.DAYS[int(dow)])
+      translated_days.append(self.DAYS[int(dow)])
     if (len(translated_days) == 0):
       return 'None'
     return ','.join(translated_days)
 
-  def set_schedule(self, days, start_time, end_time, start_enabled=True, end_enabled = True):
+  def set_time(self, days, start_time, end_time, start_enabled=True, end_enabled = True):
     for day in days:
       if day in self.DAYS:
         self.attributes['map'][str(int(self.DAYS.index(day)))] = str(int(days[day]))
@@ -39,7 +35,7 @@ class Program:
       else:
         print(f'Unknown day {day}')
 
-  def reset_schedule(self):
+  def reset(self):
     for day in self.DAYS:
       self.attributes['map'][str(self.DAYS.index(day))] = '0'
       self.attributes['end']['enable'] = '0'
@@ -47,13 +43,57 @@ class Program:
       self.attributes['start']['enable'] = '0'
       self.attributes['start']['time'] = '00:00'
 
+  def to_json(self):
+    return {
+      "start": self.attributes['start'],
+      "end": self.attributes['end'],
+      "map": self.attributes['map']
+    }
+
   def __repr__(self):
     days = self.translate_days_value(self.attributes['map'])
     start_time = self.attributes['start']['time']
     start_enabled = bool(int(self.attributes['start']['enable']))
     end_time = self.attributes['end']['time']
     end_enabled = bool(int(self.attributes['end']['enable']))
-    return "Program %s (%s): %s(%s) to %s(%s) on %s" % (self.id, self.name, start_time, start_enabled, end_time, end_enabled, days)
+    return "\n%s(%s) to %s(%s) on %s" % (start_time, start_enabled, end_time, end_enabled, days)
+
+class Program:
+  id = ''
+  name = ''
+  time_slots = []
+  device = None
+
+  def __init__(self, id, name, attributes, device):
+    self.id = id
+    self.name = name
+    self.device = device
+    self.time_slots = []
+    for time_slot in attributes:
+      self.time_slots.append(TimeSlot(time_slot))
+
+  def to_json(self):
+    slots = []
+    for time_slot in self.time_slots:
+      slots.append(time_slot.to_json)
+    return {
+      "id": self.id,
+      "name": self.name,
+      "program": slots
+    }
+
+  def save(self):
+    data = { 
+      "token": self.device.timeguard.token,
+      "program": self.to_json,
+      "user_id": self.device.timeguard.user_id,
+      "wifi_box_id": self.id,
+    }
+    pprint(data)
+    #response_json = self.device.timeguard.api_request('PUT','wifi_boxes/program', data)
+
+  def __repr__(self):
+    return "Program %s (%s):\n  %s" % (self.id, self.name, self.time_slots)
 
 class Device:
   name = ''
@@ -80,17 +120,13 @@ class Device:
     device_json = self.timeguard.api_request('GET',f'wifi_boxes/data/user_id/{self.timeguard.user_id}/wifi_box_id/{self.id}/token/{self.timeguard.token}')
     message = device_json['message']
     program_list_json = self.timeguard.api_request('GET',f'wifi_boxes/program_list/user_id/{self.timeguard.user_id}/wifi_box_id/{self.id}/token/{self.timeguard.token}')
-    program_list = {}
-    for program_name in program_list_json['message']['namelist']:
-      program_list[program_name['id']] = program_name['name']
+    for program in program_list_json['message']['namelist']:
+      program_json = self.timeguard.api_request('GET',f'wifi_boxes/program/user_id/{self.timeguard.user_id}/wifi_box_id/{self.id}/index/{program["id"]}/token/{self.timeguard.token}')
+      self.programs.append(Program(program['id'], program['name'], program_json['message']['program'], self))
     self.holiday = bool(int(message['holiday']['enable']))
     self.holiday_start  = message['holiday']['start']
     self.holiday_end  = message['holiday']['end']
     self.advance = bool(int(message['advance']))
-    for program in message['program']:
-      id = str(message['program'].index(program))
-      program_name = program_list[id]
-      self.programs.append(Program(id, program_name, program))
 
   def __repr__(self):
     return f'{self.name}'
@@ -105,12 +141,13 @@ class TimeGuard:
   base_url = ''
   token = ''
   user_id = ''
+  HEADERS = {'User-Agent': 'okhttp/3.3.1'}
+  BASE_URL = 'https://www.cloudwarm.net/TimeGuard/api/Android/v_1'
 
   def __init__(self, config_path=f'{str(Path.home())}/.timeguard.yaml', cache_folder=f'{os.getcwd()}/cache'):
     self.config = self.read_config(config_path)
     self.validate_config()
     self.cache_folder = cache_folder
-    self.base_url = 'https://www.cloudwarm.net/TimeGuard/api/Android/v_1'
 
   def read_config(self, config_path):
     """ Read the configuration file """
@@ -132,12 +169,11 @@ class TimeGuard:
     if self.config['use_cache']:
       with open(f'{self.cache_folder}/{cache_file}') as data_file:    
         return json.load(data_file)
-    headers = {'User-Agent': 'okhttp/3.3.1'}
     timeout = (self.connect_timeout, self.read_timeout)
     if type == 'PUT':
-      response = requests.put(f'{self.base_url}/{uri}', headers=headers, data=data, timeout=timeout)
+      response = requests.put(f'{self.BASE_URL}/{uri}', data=data, headers=self.HEADERS, timeout=timeout)
     elif type == 'GET':
-      response = requests.get(f'{self.base_url}/{uri}', headers=headers, timeout=timeout)
+      response = requests.get(f'{self.BASE_URL}/{uri}', headers=self.HEADERS, timeout=timeout)
     if response.status_code == 200:
       response_json = response.json()
       status_file = open(f'{self.cache_folder}/{cache_file}', 'w')
@@ -160,16 +196,16 @@ def main():
   tg = TimeGuard()
   tg.refresh_devices()
   for device in tg.devices:
-    pprint(device.name)
+    print(device.name, device.id)
     for program in device.programs:
-     everyday = { 'Mon': True, 'Tue': True, 'Wed': True, 'Thu': True, 'Fri': True, 'Sat': True, 'Sun': True}
-     pprint(program)
-      #if program.id == 0:
-      #  program.set_schedule(everyday, '05:00', '06:00')
-      #else:
-      #   program.reset_schedule()
-      #print('After')
-      #pprint(program.attributes)
+      everyday = { 'Mon': True, 'Tue': True, 'Wed': True, 'Thu': True, 'Fri': True, 'Sat': True, 'Sun': True}
+      if program.id == '0':
+        program.time_slots[0].set_time(everyday, '05:00', '06:00')
+      else:
+        program.name = f'Program {program.id}'
+        program.time_slots[0].reset()
+      print(program.id, program.name)
+      print(program.time_slots)
 
 if __name__ == "__main__":
   main()
